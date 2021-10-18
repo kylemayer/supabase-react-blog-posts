a4bc38b8f57238d9deb5aeb91d4646d9ce911928
Kyle Mayer <x@kylemayer.dev>
2021-10-18 13:47:25 -0700

    supabase connected, posts render

---
+++ src/client.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+    "https://pynxlwmnpyluusxfrhxz.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNDU4NzYxMCwiZXhwIjoxOTUwMTYzNjEwfQ.Ov2yptS3mBYcgo76ohFM4uwDoKdn_574vbbaO8rGbH4"
+
+)
